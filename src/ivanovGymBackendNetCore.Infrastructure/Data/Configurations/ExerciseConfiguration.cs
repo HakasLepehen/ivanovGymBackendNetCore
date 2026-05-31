d2a8b696cf97ca7bbf07d5ea9773673cafbf2933
Pavel Petrov <doctorrosen2016@gmail.com>
2026-05-31 17:04:56 +0600

    added new migration

--- src/ivanovGymBackendNetCore.Infrastructure/Data/Configurations/ExerciseConfiguration.cs
+++ src/ivanovGymBackendNetCore.Infrastructure/Data/Configurations/ExerciseConfiguration.cs
@@ -33,8 +33,8 @@ public class ExerciseConfiguration : IEntityTypeConfiguration<Exercise>
 
         builder.Property(e => e.MuscleGroup)
             .HasColumnName("muscle_group")
-            .HasColumnType("text")
-            .HasDefaultValue("")
+            .HasColumnType("int")
+            .HasDefaultValue(null)
             .IsRequired(false);
     }
 }
---
+++ src/ivanovGymBackendNetCore.Infrastructure/Migrations/20260531110221_addedExerciseEntity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ivanovGymBackendNetCore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedExerciseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "exercises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    url = table.Column<string>(type: "text", nullable: true, defaultValue: ""),
+                    comment = table.Column<string>(type: "text", nullable: true, defaultValue: ""),
+                    muscle_group = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_exercises", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "exercises");
+        }
+    }
+}
