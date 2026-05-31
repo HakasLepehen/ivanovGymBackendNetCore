a9500b8d92c54ae91e6464aee3d6a0453a8b7cd9
Pavel Petrov <doctorrosen2016@gmail.com>
2026-05-31 22:59:14 +0600

    imported data from supabase

--- src/ivanovGymBackendNetCore.Application/Profiles/MappingProfile.cs
+++ src/ivanovGymBackendNetCore.Application/Profiles/MappingProfile.cs
@@ -15,5 +15,6 @@ public class MappingProfile : Profile
         CreateMap<Client, ClientDto>();
         CreateMap<ClientDto, Client>();
         CreateMap<ExerciseDto, Exercise>();
+        CreateMap<Exercise, ExerciseDto>();
     }
 }
