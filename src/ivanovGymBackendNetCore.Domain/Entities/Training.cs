2487c1c7c22078a4dd387be6311f0c053b8ae5a2
Pavel Petrov <doctorrosen2016@gmail.com>
2026-06-13 00:01:12 +0600

    added fields to Training entity

--- src/ivanovGymBackendNetCore.Domain/Entities/Training.cs
+++ src/ivanovGymBackendNetCore.Domain/Entities/Training.cs
@@ -2,4 +2,10 @@ namespace ivanovGymBackendNetCore.Domain.Entities;
 
 public class Training
 {
+    public int Id { get; set; }
+    public Guid ClientGuid { get; set; }
+    public byte Hour { get; set; }
+    public byte Minutes { get; set; }
+    public DateTime PlannedDate { get; set; }
+    public bool IsCompleted { get; set; }
 }
