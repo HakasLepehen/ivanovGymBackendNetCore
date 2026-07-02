9527a7bb78b7f2e77662af4dabf386e639884983
petrovpv <petrovpv@competentit.ru>
2026-07-02 15:32:23 +0600

    added new entity

---
+++ src/ivanovGymBackendNetCore.Domain/Entities/TrainingExercise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ivanovGymBackendNetCore.Domain.Entities;
+
+class TrainingExercise
+{
+    public int Id { get; set; }
+    public int TrainingId { get; set; }
+    public int ExerciseId { get; set; }
+    public byte? ExecutionNumber { get; set; } = 0;
+    public string? SetCount { get; set; } = "";
+    public string? PayloadWeight { get; set; } = "";
+    public string? Comment { get; set; } = "";
+}
